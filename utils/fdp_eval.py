5d4f29c5cb8d598bf0e5aacc361afb117c542816
Minxing-Zheng <minxing.zheng1@gmail.com>
2025-12-02 21:41:53 -0500

    Demo Notebook Update
    Update CP, notebook supports true set size comparison with predicted set size, and output predicted doc_IDs

--- utils/fdp_eval.py
+++ utils/fdp_eval.py
@@ -3,25 +3,30 @@ import numpy as np
 def compute_fdp(data_dict, lambda_value, label_threshold=3):
     fdp_dict = {}
     selected, n_true = [], []
-
+    selected_doc_ids = {}
     for qid, docs in data_dict.items():
+        doc_ids = np.array([d[0] for d in docs])
         labels = np.array([d[1] for d in docs])
         scores = np.array([d[2] for d in docs])
 
         mask = scores >= lambda_value
         s = mask.sum()
         t = (mask & (labels >= label_threshold)).sum()
+        true_count = ((labels >= label_threshold)).sum()
+        # t = ((labels >= label_threshold)).sum()
         fdp = (s - t) / s if s > 0 else 0.0
 
         fdp_dict[qid] = fdp
         selected.append(s)
-        n_true.append(t)
+        n_true.append(true_count)
+        selected_doc_ids[qid] = doc_ids[mask].tolist()
 
     return fdp_dict, {
         "mean_fdp": float(np.mean(list(fdp_dict.values()))),
         "mean_selected": float(np.mean(selected)),
         "mean_true": float(np.mean(n_true)),
-        "per_sample_fdp": list(fdp_dict.values())
+        "per_sample_fdp": list(fdp_dict.values()),
+        "predicted_doc_ids": selected_doc_ids,     
     }
 
 
@@ -42,4 +47,3 @@ def find_lambda_hat(calib_dict, delta, alpha, step=0.001):
         else:
             break
     return best
-
