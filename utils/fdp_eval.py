36c0b243f8d87eb2ac5ef64eb5ba5bcf3e9de33e
Minxing-Zheng <minxing.zheng1@gmail.com>
2025-12-02 23:19:14 -0500

    Update fdp_eval.py

--- utils/fdp_eval.py
+++ utils/fdp_eval.py
@@ -1,32 +1,48 @@
 import numpy as np
 
+
 def compute_fdp(data_dict, lambda_value, label_threshold=3):
     fdp_dict = {}
     selected, n_true = [], []
-    selected_doc_ids = {}
+    predicted_doc_ids = {}
+    predicted_doc_scores = {}
+
     for qid, docs in data_dict.items():
         doc_ids = np.array([d[0] for d in docs])
-        labels = np.array([d[1] for d in docs])
-        scores = np.array([d[2] for d in docs])
+        labels  = np.array([d[1] for d in docs])
+        scores  = np.array([d[2] for d in docs])
 
         mask = scores >= lambda_value
+
+        # FDP components
         s = mask.sum()
         t = (mask & (labels >= label_threshold)).sum()
-        true_count = ((labels >= label_threshold)).sum()
-        # t = ((labels >= label_threshold)).sum()
+        true_count = (labels >= label_threshold).sum()
         fdp = (s - t) / s if s > 0 else 0.0
 
         fdp_dict[qid] = fdp
         selected.append(s)
         n_true.append(true_count)
-        selected_doc_ids[qid] = doc_ids[mask].tolist()
+
+        # ----- Sort selected docs by score (descending) -----
+        sel_ids = doc_ids[mask]
+        sel_scores = scores[mask]
+
+        if sel_ids.size > 0:
+            order = np.argsort(-sel_scores)  # descending
+            sel_ids = sel_ids[order]
+            sel_scores = sel_scores[order]
+
+        predicted_doc_ids[qid] = sel_ids.tolist()
+        predicted_doc_scores[qid] = sel_scores.tolist()
 
     return fdp_dict, {
         "mean_fdp": float(np.mean(list(fdp_dict.values()))),
         "mean_selected": float(np.mean(selected)),
         "mean_true": float(np.mean(n_true)),
         "per_sample_fdp": list(fdp_dict.values()),
-        "predicted_doc_ids": selected_doc_ids,     
+        "predicted_doc_ids": predicted_doc_ids,
+        "predicted_doc_scores": predicted_doc_scores,
     }
 
 
